6843cafd0f6a8cabec7fa9c120a8d9274a73b0b2
Emmanuel NDAHIMANA <46503359+EmmanuelNDAHIMANA@users.noreply.github.com>
2020-01-21 15:14:59 +0000

    Add files via upload

--- MunichChainLadderAnalysis.R
+++ MunichChainLadderAnalysis.R
@@ -3,7 +3,7 @@ library(ChainLadder)
 library(tidyverse)
 # Using our datain munich chain ladder model, the data should be in triangle format.
 #We start by importing our csv file into a dataframe
-df <- read.csv2("finaldataP.csv")
+df <- read.csv2("D:/Project/Rdata&codes/finaldataP.csv")
 #From the imported dataframe df, we extract the incurred data as run-off triangle.
 INCURTriGL1P <-  as.triangle(df, origin="AccidentYear", dev = "Development", value="IncurredValue")
 #And again, from the dataframe df, we retrieve the paid loss claims data in triangle format here below
@@ -22,18 +22,24 @@ plot(MCL)
 MCL$lambdaP
 MCL$lambdaI
 #The conditional deviations plots in figure 4.3 and figure 4.4 can be ploted like this.
-plot(MCL$rhoP.sigma, type="l", col="blue", lwd=3, ylab="rhoP.sigma", xlab="development periods",
-     main="Paid conditional deviations vs development periods")
-plot(MCL$rhoI.sigma, type="l", col="blue", lwd=3, ylab="Incurred factors", xlab="development periods",
-     main="Incurred conditional deviations vs development periods")
+plot(MCL$rhoP.sigma, type="l", col="blue",lty=1, lwd=3, ylab="Deviations", xlab="development periods")
+lines(MCL$rhoI.sigma, type="l", col="red",lty=2, lwd=3, ylab="Incurred factors", xlab="development periods")
+legend("topright", lty = c(1,2),col = c("blue","red"), lwd = c(3,3), legend =c("Paid deviations","incurred deviations"))
+
 #The plots of chain ladder-link ag-to-age factors in figure 4.5 and figure 4.6 can be ploted like this.
-plot(MCL$q.f, type="l", col="blue", lwd=3, ylab="qfactors", xlab="development periods", 
-     main="chain-ladder-link age-to-age factors of the paid/incurred triangle vs development periods")
-plot(MCL$qinverse.f, type="l", col="blue", lwd=3, ylab="qinverse factors", xlab="development periods", 
-     main="chain-ladder-link age-to-age factors of the incurred/paid triangle vs development periods")
+plot(MCL$qinverse.f, type="l", col="red", ylim=c(0.1,4),lwd=3, pch=2, lty=3, ylab="age-to-age factors", xlab="development periods")
+lines(MCL$q.f, type="l", col="blue", lwd=3, ylab="age-to-age factor", xlab="development periods")
+abline(h=1, lty=2, col="black")
+legend("topright", lty = c(3,1),col = c("red","blue"), lwd = c(3,3), legend =c("Incurred/Paid age-to-age factors","Paid/Incurred age-to-age factors"))
+
+
+
 #The weighted average age-to-age factors in figure 4.7 and figure 4.8 can be ploted like this.
-plot(MCL$MackPaid$f , type="o", col="blue", lwd=3, ylab="Paid factors", xlab="development periods")
-plot(MCL$MackIncurred$f , type="o", col="blue", lwd=3, ylab="Incurred factors", xlab="development periods")
+plot(MCL$MackPaid$f , type="l", col="red", lty=2,lwd=3, ylab="Weighted average age-to-age factors", xlab="development periods")
+lines(MCL$MackIncurred$f , type="o", col="blue", lwd=3, ylab="Incurred factors", xlab="development periods")
+legend("topright", lty = c(3,1),col = c("red","blue"), lwd = c(3,3), legend =c("Paid factors","Incurred factors"))
+
 #Munich-chain-ladder forecasted full triangles on paid and incurred data in table 6.1 and table 6.2 can be obtained like this.
 MCL$MCLPaid
 MCL$MCLIncurred
+
